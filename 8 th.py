127e668f4ed20da8273cb4ab5d15f21d490b9c59
dakeaditya <149558735+dakeaditya@users.noreply.github.com>
2023-11-01 13:01:01 +0530

    Add files via upload

---
+++ 8 th.py
@@ -0,0 +1,16 @@
+#list
+list=['a','b','c','d']
+print(list1)
+print(len(list1))
+
+list1=['a','b','c','d','e','f','g','h']
+list2=[1,2,3,4,5]
+list3=[True,False,False]
+
+print(list1)
+print(list2)
+print(list3)
+
+#Access Items
+print(list1[3])
+
